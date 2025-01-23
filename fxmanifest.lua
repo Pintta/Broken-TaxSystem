3004c89f012d8a2fc25df414d3189c2481ba4ff5
Pintta <69728770+Pintta@users.noreply.github.com>
2025-01-23 22:20:38 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+author 'https://github.com/Pintta/Broken-TaxSystem'
+description 'Broken-TaxSystem'
 client_script 'client.lua'
 server_scripts {
     'install.lua', -- After installing, you can remove this line (I recommend)
